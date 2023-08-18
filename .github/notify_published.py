fc24e15104835ea2bdb338caf66100e68541b8e1
Travja <the.only.t.craft@gmail.com>
2023-08-17 23:19:25 -0600

    Actually handle release version downloads properly

--- .github/notify_published.py
+++ .github/notify_published.py
@@ -22,8 +22,9 @@ def get_info():
 
 version, name, url = get_info()
 if not is_dev:
-    url = url.replace('https://s01.oss.sonatype.org:443/service/local/staging/deployByRepositoryId/compromcteam-1002',
-                  'https://s01.oss.sonatype.org/service/local/repositories/releases/content')
+    url = re.sub('https:\/\/s01\.oss\.sonatype\.org:443\/service\/local\/staging\/deployByRepositoryId\/compromcteam-\d+',
+                  'https://s01.oss.sonatype.org/service/local/repositories/releases/content',
+                 url)
 embed = {
     'author': {
         'name': 'New ' + ('Dev ' if is_dev else '') + 'Build Available!',
