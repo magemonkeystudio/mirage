c52fc192acb05a0182cf25b8238b3fd087e7a659
Trav <the.only.t.craft@gmail.com>
2025-11-09 02:37:38 -0700

    Update dependencies

--- src/main/java/studio/magemonkey/mirage/requirements/FusionProfessionRequirement.java
+++ src/main/java/studio/magemonkey/mirage/requirements/FusionProfessionRequirement.java
@@ -2,9 +2,9 @@ package studio.magemonkey.mirage.requirements;
 
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import studio.magemonkey.fusion.CraftingTable;
-import studio.magemonkey.fusion.LevelFunction;
-import studio.magemonkey.fusion.cfg.Cfg;
+import org.joml.Math;
+import studio.magemonkey.fusion.cfg.ProfessionsCfg;
+import studio.magemonkey.fusion.data.recipes.CraftingTable;
 
 public class FusionProfessionRequirement extends Requirement {
     public static final String NAME = "FUSION_profession";
@@ -21,7 +21,7 @@ public class FusionProfessionRequirement extends Requirement {
         } else if (split.length == 3) {
             this.level = Math.max(1, Integer.parseInt(split[2]));
         } else throw new IllegalArgumentException();
-        CraftingTable profession = Cfg.getTable(split[1]);
+        CraftingTable profession = ProfessionsCfg.getTable(split[1]);
         if (profession == null) throw new IllegalArgumentException("Unknown profession \"" + split[1] + '\"');
         this.profession = profession;
     }
@@ -34,6 +34,6 @@ public class FusionProfessionRequirement extends Requirement {
 
     @Override
     public boolean meets(@NotNull Player player) {
-        return LevelFunction.getLevel(player, profession) >= this.level;
+        return profession.getLevelFunction().getLevel(player) >= this.level;
     }
 }
